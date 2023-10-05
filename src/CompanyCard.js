f7ec302411fca8b212f59c2431c34ecf7c5670ef
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-10-04 21:13:19 -0600

    add api file

---
+++ src/CompanyCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CompanyCard() {
+    return (
+        <div>
+            <h2>This is a company card component</h2>
+        </div>
+    );
+}
+
+
+export default CompanyCard;
